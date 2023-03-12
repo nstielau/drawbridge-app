17ddc82cf451b3a1e173a0478ef4c27bd5427b04
Nick Stielau <nick.stielau@gmail.com>
2023-03-11 22:10:33 -0800

    Syncing connected state.

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -25,21 +25,34 @@
       bordered
     >
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
+        <q-item-label header>
+          Settings
         </q-item-label>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item>
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Domain Name</q-item-label>
+            <q-item-label caption>{{ fqdn || "No FQDN set"}}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable @click="clearFQDN">
+          <q-item-section avatar>
+            <q-icon name="calendar_today" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Clear FQDN</q-item-label>
+          </q-item-section>
+        </q-item>        
       </q-list>
     </q-drawer>
 
-    <q-page-container>
+    <q-page-container key="fqdn">
       <router-view />
     </q-page-container>
   </q-layout>
@@ -47,70 +60,31 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
       }
     }
-  }
+  },
+  data() {
+    return {
+      fqdn: localStorage.getItem('fqdn')
+    }
+  },
+  methods: {
+    clearFQDN() {
+      this.fqdn= null;
+      localStorage.removeItem("fqdn");
+    }
+  },
 })
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex justify-center item-start">
     <div v-if="!fqdn">
       <div class="row" style="padding-top: 10rem;">
         <h3>Please set domain name.</h3>
@@ -10,23 +10,32 @@
       </div>
     </div>
     <div v-else>
+      <div v-if="isOpen" class="row justify-center">
+        <h4>Gate is Open</h4>
+      </div>
+      <div v-else class="row">
+        <h4>Gate is Closed</h4>
+      </div>
       <div class="row">
         <q-btn class="q-ma-lg q-px-lg q-py-md" 
-               :loading="loading"
+               :loading="isWaitingResponse"
                color="warning"
                label="Open 1m"
                @click="open(60)"/>
         <q-btn class="q-ma-lg q-px-lg q-py-md"
+               :loading="isWaitingResponse"
                color="warning"
                label="Open 10m"
                @click="open(600)"/>      
       </div>
       <div class="row">
         <q-btn class="q-ma-lg q-px-lg q-py-md"
+               :loading="isWaitingResponse"
                color="warning"
                label="Open 30m"
                @click="open(1800)"/>
         <q-btn class="q-ma-lg q-px-lg q-py-md"
+               :loading="isWaitingResponse"
                color="warning"
                label="Open 60m"
                @click="open(3600)"/>      
@@ -44,27 +53,36 @@ export default defineComponent({
   data() {
     return {
       fqdn: localStorage.getItem('fqdn'),
-      loading: false
+      isWaitingResponse: false,
+      isOpen: false
     }
+  },
+  mounted() {
+    this.getGateStatus();
   },
   methods: {
     set_fqdn() {
       localStorage.setItem('fqdn', this.$refs.fqdn_input.value);
       this.fqdn = this.$refs.fqdn_input.value;
     },
+    getGateStatus() {
+      axios.request('https://' + this.fqdn + '/open', {
+        method: 'GET'
+      }).then(response => {
+        console.log("Gate is open: ", response.data.connected)
+        this.isOpen = response.data.connected;
+      }).catch(error => console.log("Error loading gate", error));
+    },
     open(seconds) {
-      this.loading = true;
-
+      this.isWaitingResponse = true;
       axios.request('https://' + this.fqdn + '/open', {
         method: 'POST',
         data: seconds
       }).then(response => {
         console.log("Success opening gate", response)
-        this.loading = false;
-      }).catch(error => {
-        console.log("Error opening gate", error);
-        this.loading = false;
-      });
+        this.isWaitingResponse = false;
+        this.isOpen = true;
+      }).catch(error => console.log("Error opening gate", error));
     } 
   }
 })
